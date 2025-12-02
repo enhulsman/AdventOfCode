bcc119ea2342de0c2ae288bf5e9e91616a848bfc
Ezra Hulsman <ezra@hulsman.dev>
2025-12-02 15:36:04 +0100

    Solution 1

--- 2024/1-HistorianHysteria/solution.py
+++ 2024/1-HistorianHysteria/solution.py
@@ -1,5 +1,30 @@
 def solution_part1():
+    file = open("input.txt", "r")
+    line = file.readline().strip()
+
+    left = []
+    right = []
+
+    while line:
+        line = line.split("   ")
+        left.append(int(line[0]))
+        right.append(int(line[1]))
+
+        line = file.readline().strip()
+    
+    left.sort()
+    right.sort()
+
+    res = 0
+
+    for i in range(len(left)):
+        res += abs(left[i] - right[i])
+
+    print(res)
+
+def solution_part2():
     pass
 
 if __name__ == "__main__":
     solution_part1()
+    solution_part2()
